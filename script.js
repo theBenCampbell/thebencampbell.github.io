ebb9e2d6dea335f1749fc8a3b855a5269635196c
Ben Campbell <b3ncampbell@users.noreply.github.com>
2017-04-03 16:29:13 +0100

    CSS changes

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ $(document).ready(function() {
 	$("body").hide();
 	$("body").fadeIn(1600);
 });
+
+$("a[href='#bottom']").click(function() {
+  $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+  return false;
+});
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
