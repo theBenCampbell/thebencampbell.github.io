528f679866d3fc8d268e12b43d7f6c4337dcaa5a
Ben Campbell <b3ncampbell@users.noreply.github.com>
2017-04-25 21:32:19 +0100

    dropdown menu

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ $("a[href='#top']").click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
+
+// Show/hide dropdown menu when "Navigation" is clicked
+function menuShow() {
+    document.getElementById("menu").classList.toggle("show");
+}
+
+// Close the dropdown menu when a link is clicked or outside of menu is clicked
+window.onclick = function(event) {
+  if (!event.target.matches('.menuButton')) {
+
+    var dropdown = document.getElementsByClassName("menuContent");
+    var i;
+    for (i = 0; i < dropdown.length; i++) {
+      var openDropdown = dropdown[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
